7e295705b237fdfa9a48012d18f6e9b962e3f9aa
DNature <divinehycenth@outlook.com>
2019-07-11 16:46:11 +0100

    Updates

---
+++ precache-manifest.b446d75d9b54a1a1e33d28f91a09ea0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7140f8234e015556232afa4805a0b4d",
+    "url": "/react-pagination/index.html"
+  },
+  {
+    "revision": "a9f1bea6319ab9743002",
+    "url": "/react-pagination/static/css/main.82c3c170.chunk.css"
+  },
+  {
+    "revision": "4dea63ec98f4a4958f92",
+    "url": "/react-pagination/static/js/2.02cc656a.chunk.js"
+  },
+  {
+    "revision": "a9f1bea6319ab9743002",
+    "url": "/react-pagination/static/js/main.2651563e.chunk.js"
+  },
+  {
+    "revision": "6bb6875cc2e032a389e0",
+    "url": "/react-pagination/static/js/runtime~main.f8e684e0.js"
+  }
+]);
